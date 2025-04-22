4c84ae00a26f1cd9e5b19b7cf4283c1d0b4a4834
kipkoechke <chepkwonyke1@gmail.com>
2025-04-22 16:31:50 +0300

    Chore: refactor controllers and modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/db.module';
-import { PdfController } from './modules/pdf/pdf.controller';
-import { PropertyModule } from './modules/properties/properties.module';
 import { PdfExtractorModule } from './modules/sales-comparables/sales-comparables.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
-    PropertyModule,
     PdfExtractorModule,
   ],
-  controllers: [AppController, PdfController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
